8df5808cde269ead5f3af5285c5b12761d68c473
Taykahn <marshall962000@yahoo.com>
2017-03-30 23:01:25 -0500

    Update page-sidebar-left.php

--- page-sidebar-left.php
+++ page-sidebar-left.php
@@ -1,37 +1,43 @@
 <?php 
-/*
-		Template Name: Left Sidebar
+/**
+* Template Name: Left Sidebar
 */
 ?>
 <?php get_header(); ?>
 
 <section class="two-column row no-max pad">
+	
 	<div class="small-12 columns">
-      	<div class="row">
-          <!-- Primary Column -->
-         	<div class="small-12 medium-7 medium-offset-1 medium-push-4 columns">
-            	<div class="primary">
+      	
+		<div class="row">
+          
+			<!-- Primary Column -->
+         		
+			<div class="primary">
 
-			        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+				<div class="small-12 medium-7 medium-offset-1 medium-push-4 columns">
 
-			          <h1><?php the_title(); ?></h1>
-			          <p><?php the_content(); ?></p>
+					<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-			        <?php endwhile; else : ?>
+						  <h1><?php the_title(); ?></h1>
+						  <p><?php the_content(); ?></p>
+
+					<?php endwhile; else : ?>
 
 						<p><?php _e( 'Sorry, no pages found.' ); ?></p>
 
 					<?php endif; ?> 
 
-			</div>
-		</div> 
+				</div><!-- small-12 medium-7 medium-offset-1 medium-push-4 columns -->
+			
+			</div><!-- end primary --> 
 		
-<?php get_sidebar( 'page' ); ?>
+		<?php get_sidebar( 'page' ); ?>
 
- </div>
-</section>
+		</div><!-- end row -->
+
+	</div><!-- small-12 columns -->
+		
+</section><!-- two-column row no-max pad -->
               
-
-
-
 <?php get_footer(); ?>
